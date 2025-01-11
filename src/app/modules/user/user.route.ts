c3c9cf729da63a6b8db255ba5344525e405111c1
shohan-pherones <shohan.sub.56@gmail.com>
2025-01-11 22:56:07 +0600

    add register schema for validation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
 import { UserControllers } from "./user.controller";
+import { validate } from "../../middlewares/validation.middleware";
+import { UserValidations } from "./user.validation";
 
 const router: Router = express.Router();
 
-router.post("/auth/register", UserControllers.register);
+router.post(
+  "/auth/register",
+  validate(UserValidations.registerSchema),
+  UserControllers.register
+);
 
 export default router;
